a8980d29490c498b27cf99a0f7f42f4d5d1e72b7
Kauan <159711116+WillsKFG@users.noreply.github.com>
2025-09-30 22:29:40 -0300

    .

---
+++ teste lambda.py
@@ -0,0 +1,8 @@
+def multiplicacao(x, y):
+    mult = x*y
+    return mult
+
+multiplicacao02 = lambda a, b: a*b
+
+print(multiplicacao(5, 6))
+print(multiplicacao02(4, 5))
---
+++ teste2.py
@@ -0,0 +1,8 @@
+import random
+cores = ['azul', 'vermelho', 'amarelo']
+escolha = random.choice(cores)
+print(cores)
+print(escolha)
+
+cores.append("rosa")
+print(cores)
